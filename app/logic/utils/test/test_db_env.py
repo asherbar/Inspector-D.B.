6c635d8404ac48af6aa730069dac0bdf4b7586a3
asherbar <asherbare@gmail.com>
2019-02-19 07:53:43 +0200

    Added option: DB_CREDENTIALS

--- app/logic/utils/test/test_db_env.py
+++ app/logic/utils/test/test_db_env.py
@@ -30,10 +30,10 @@ class TestInspectorDbEnv(unittest.TestCase):
         db_name_bad = 'bla'
         db_name = 'DB'
         username = 'UN'
-        password = 'P'
+        pswd = 'P'
         hostname = 'HN'
         port = 1234
-        good_credentials = f'{{"dbname":"{db_name}","hostname":"{hostname}","password":"{password}","port":{port},' \
+        good_credentials = f'{{"dbname":"{db_name}","hostname":"{hostname}","password":"{pswd}","port":{port},' \
                            f'"username":"{username}"}}'
         bad_credentials = f'{{"dbname":"{db_name_bad}"}}'
         with mock.patch.dict('os.environ', {'DB_CREDENTIALS': f'[{good_credentials}, {bad_credentials}]'}):
@@ -50,12 +50,12 @@ class TestInspectorDbEnv(unittest.TestCase):
     def test_get_bound_db_names_not_list(self):
         db_name = 'DB'
         username = 'UN'
-        password = 'P'
+        pswd = 'P'
         hostname = 'HN'
         port = 1234
         with mock.patch.dict('os.environ',
                              {'DB_CREDENTIALS': f'{{"dbname":"{db_name}","hostname":"{hostname}",'
-                                                f'"password":"{password}","port":{port},"username":"{username}"}}'}):
+                                                f'"password":"{pswd}","port":{port},"username":"{username}"}}'}):
             object_under_test = DbEnv()
             db_names = object_under_test.get_db_names()
             self.assertEqual(db_names, [])
@@ -63,10 +63,10 @@ class TestInspectorDbEnv(unittest.TestCase):
     def test_get_bound_db_names_missing_credentials_key(self):
         db_name = 'DB'
         username = 'UN'
-        password = 'P'
+        pswd = 'P'
         hostname = 'HN'
         port = 1234
-        good_credentials = f'{{"dbname":"{db_name}","hostname":"{hostname}","password":"{password}","port":{port},' \
+        good_credentials = f'{{"dbname":"{db_name}","hostname":"{hostname}","password":"{pswd}","port":{port},' \
                            f'"username":"{username}"}}'
         with mock.patch.dict('os.environ', {'VCAP_SERVICES': f'{{"postgresql": [{{"CREDS": {good_credentials} }}] }}'}):
             object_under_test = DbEnv()
